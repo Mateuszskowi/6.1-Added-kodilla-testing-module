0ad7aa5a513d81d4893107ada72597ed5fbf9559
Mateuszskowi <mateuszskowi@gmail.com>
2022-03-30 21:16:51 +0200

    6.1 Added kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -7,7 +7,11 @@ public class TestingMain {
         int result1 = calculator.add(1,2);
         int result2= calculator.subtract(3,4);
 
-        if(result1.equ)
+        if((result1 == 3) && (result2 == -1)){
+            System.out.println("Calculator is ok");
+        } else{
+            System.out.println("Calculator is not ok");
+        }
 
 
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
