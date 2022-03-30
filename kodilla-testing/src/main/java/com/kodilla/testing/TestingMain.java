a444f8b5bbb3455a1354d7c86f5318e49847c30e
Mateusz Skowronek <mateuszskowi@gmail.com>
2022-03-30 18:29:21 +0200

    6.1 Added kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,15 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
+        Calculator calculator =new Calculator();
+        int result1 = calculator.add(1,2);
+        int result2= calculator.subtract(3,4);
+
+        if(result1.equ)
+
+
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
     }
 }
