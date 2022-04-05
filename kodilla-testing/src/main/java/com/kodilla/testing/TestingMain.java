73118053b0c4d160d80fe4decbee153f84e3a26d
Mateuszskowi <mateuszskowi@gmail.com>
2022-04-05 11:58:25 +0200

    6.1 Added kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,13 +3,13 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calculator =new Calculator();
-        int result1 = calculator.add(1,2);
-        int result2= calculator.subtract(3,4);
+        Calculator calculator = new Calculator();
+        int result1 = calculator.add(1, 2);
+        int result2 = calculator.subtract(3, 4);
 
-        if((result1 == 3) && (result2 == -1)){
+        if ((result1 == 3) && (result2 == -1)) {
             System.out.println("Calculator is ok");
-        } else{
+        } else {
             System.out.println("Calculator is not ok");
         }
 
